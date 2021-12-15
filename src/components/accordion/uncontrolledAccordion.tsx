2c8fbadda6943fd6c805e6fa764d3ca33832ac3c
FoxSide <foxside8452@gmail.com>
2021-12-15 16:25:57 +0200

    16-ln

--- src/components/accordion/uncontrolledAccordion.tsx
+++ src/components/accordion/uncontrolledAccordion.tsx
@@ -1,15 +1,26 @@
-import React, {useState} from "react";
+import React, {useReducer, useState} from "react";
 
 type AccordionPropsType = {
   title: string
 }
+type ActionType = {
+  type: string
+}
+
+const reducer = (state: boolean, action: ActionType) => {
+  if (action.type === 'toggle-collapsed') {
+    return !state
+  }
+  return state
+}
 
 export function UncontrolledAccordion(props: AccordionPropsType) {
 
-  let [collapsed, setCollapsed] = useState(true)
+  // const [collapsed, setCollapsed] = useState(true)
+  const [collapsed, dispatch] = useReducer(reducer, false)
 
   return <div>
-    <AccordionTitle title={props.title} onClick={() => {setCollapsed(!collapsed)}}/>
+    <AccordionTitle title={props.title} onClick={() => {dispatch({type: 'toggle-collapsed'})}}/>
     {collapsed && <AccordionBody/>}
   </div>
 }
